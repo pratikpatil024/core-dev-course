fff5b1343ee8c628028c27a4a5aeaa5cfe3c7e81
Pratik Patil <pratikspatil024@gmail.com>
2022-07-24 12:46:05 +0530

    added separate file for lint CI, added lint errors in test.go

--- test.go
+++ test.go
@@ -1,5 +1,18 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
+
+	x := 0
+
+	if x == 1 {
+		x = 0
+	}
+	z := x + 2
+
+	fmt.Println(z)
+	y := "x"
+	fmt.Println(y)
 }
